a0a46741044c7eab4097b6e2eaaa3cdd59c2430a
Brendan McShane <brendanmcs7@gmail.com>
2023-01-01 19:29:41 -0500

    Add comment

--- BoringTunTest/LoggingTest.cs
+++ BoringTunTest/LoggingTest.cs
@@ -5,9 +5,10 @@
 		/// <summary>
 		/// Basic logging method. Tests using dynamic functions and avoiding GC.
 		/// </summary>
-		/// <param name="msg"></param>
+		/// <param name="msg">The MSG from wireguard</param>
 		public void logDelegateTest(string msg)
 		{
+			// Write the msg
 			Console.WriteLine(msg);
 		}
 	}
